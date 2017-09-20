cebab8db1e660a87cfe16abdbdeb50f1578ec977
bruce_b_yang@yahoo.com <bruce_b_yang@yahoo.com>
2017-09-20 19:03:04 -0400

    Added 3 activies for Lab 2

--- app/src/main/java/com/example/bruce/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/bruce/androidlabs/ListItemsActivity.java
@@ -2,32 +2,41 @@ package com.example.bruce.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME,"In onCreate()");
     }
 
     protected void onResume(){
         super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
     }
 
     protected void onStart(){
         super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
     }
 
     protected void onPause(){
         super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
     }
 
     protected void onStop(){
         super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
     }
 
     protected void onDestroy(){
         super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 }
