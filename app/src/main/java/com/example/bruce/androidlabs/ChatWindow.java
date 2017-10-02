03eb23a93b2ac79fdd74a4c4ce3cdf77f9ae71cc
bruce_b_yang@yahoo.com <bruce_b_yang@yahoo.com>
2017-10-02 15:59:39 -0400

    Oct. 2 update

--- app/src/main/java/com/example/bruce/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/bruce/androidlabs/ChatWindow.java
@@ -1,14 +1,70 @@
 package com.example.bruce.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
 
+    Button btnSend = (Button)findViewById(R.id.buttonSend);
+    EditText chatText = (EditText)findViewById(R.id.chatText);
+    ListView chatView = (ListView)findViewById(R.id.chatView);
+
+    ArrayList<String>  storedMessage = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+        final ChatAdapter messageAdapter = new ChatAdapter(this);
+        chatView.setAdapter(messageAdapter);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storedMessage.add(chatText.getText().toString());
+                messageAdapter.notifyDataSetChanged();
+                chatText.setText("");
+            }}
+        );
+
+
+    }
+
+    private class ChatAdapter extends ArrayAdapter<String>{
+        public ChatAdapter(Context ctx){
+            super(ctx,0);
+        }
+
+        public int getCount(){
+            return storedMessage.size();
+        }
+
+        public String getItem(int postion){
+            return storedMessage.get(postion);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result=null;
+            if(position%2==0)
+                result = inflater.inflate(R.layout.chat_row_incoming,null);
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing,null);
+
+            TextView message = (TextView)result.findViewById(R.id.message_text);
+            message.setText(getItem(position));
+            return result;
+        }
     }
 }
--- app/src/main/java/com/example/bruce/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/bruce/androidlabs/StartActivity.java
@@ -36,6 +36,7 @@ public class StartActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Log.i(ACTIVITY_NAME,"User clicked Start Chat");
+                Intent intent = new Intent(StartActivity.this,ChatWindow.class);
             }
         });
     }
